731a5a12fd2ca7d6ff2daf1eb911a8fe7f399dce
2DC <kkts9308@gmail.com>
2023-08-02 12:30:32 +0900

    Axios postSignIn 인스턴스 개발

---
+++ src/apis/postSignIn.ts
@@ -0,0 +1,62 @@
+import { API_PATH } from "../constants/apiPath.ts";
+import axios from "axios";
+
+const postSignIn = async (email: string, password: string) => {
+  const url = "/auth/signin";
+  const data: any = await signInInstance({
+    method: "post",
+    url: url,
+    data: {
+      email: email,
+      password: password,
+    },
+  });
+
+  const signInResult = {
+    accessToken: null,
+    message: "",
+    isSuccess: false,
+  };
+
+  if (data?.status === 200) {
+    signInResult.accessToken = data.data.access_token;
+    signInResult.message = "로그인에 성공하셨습니다.";
+    signInResult.isSuccess = true;
+  } else {
+    signInResult.message = "이메일 또는 비밀번호가 틀렸습니다.";
+    signInResult.isSuccess = false;
+  }
+
+  return signInResult;
+};
+
+const signInInstance = axios.create({
+  baseURL: API_PATH,
+  headers: {
+    "Content-type": "application/json",
+  },
+  timeout: 5000,
+});
+
+signInInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return null;
+  }
+);
+
+signInInstance.interceptors.response.use(
+  (response) => {
+    const res = response;
+    return res;
+  },
+  (error) => {
+    console.log(error);
+    return error;
+  }
+);
+
+export default postSignIn;
