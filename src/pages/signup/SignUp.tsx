3f92e1d21db2b21dbfb1106a421187d36064f135
2DC <kkts9308@gmail.com>
2023-08-02 11:43:02 +0900

    SignUp 컴포넌트 기능 구현 완료

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import usePwdValidation from "../../hooks/usePwdValidation";
 import useEmailValidation from "../../hooks/useEmailValidation";
+import postSignUp from "../../apis/postSignUp";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const navigator = useNavigate();
   const [input, setInput] = useState({
     email: "",
     password: "",
   });
   const [isEmailValid] = useEmailValidation(input.email);
   const [isPwdValid] = usePwdValidation(input.password);
+
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput({
       ...input,
@@ -16,15 +20,28 @@ export default function SignUp() {
     });
   };
 
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const signUpResult = await postSignUp(input.email, input.password);
+
+    if (signUpResult.isSuccess) {
+      alert(signUpResult.message);
+      navigator("/signin");
+    } else {
+      alert(signUpResult.message);
+    }
+  };
+
   return (
     <main>
-      <h1>로그인</h1>
-      <form>
+      <h1>회원가입</h1>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor='email'>이메일</label>
           <input
             id='email'
             name='email'
+            type='email'
             data-testid='email-input'
             onChange={handleInput}
           />
@@ -34,6 +51,7 @@ export default function SignUp() {
           <input
             id='password'
             name='password'
+            type='password'
             data-testid='password-input'
             onChange={handleInput}
           />
