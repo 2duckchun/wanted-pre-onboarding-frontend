c1a12f5e3b2d4efe47ad2d29184189f6c28cbd3f
2DC <kkts9308@gmail.com>
2023-08-03 16:38:14 +0900

    getAccessToken console.log 제거

--- src/utils/getAccessToken.ts
+++ src/utils/getAccessToken.ts
@@ -1,6 +1,5 @@
 export default function getAccessToken() {
   const access_token = localStorage.getItem("access_token");
-  console.log(access_token);
   if (access_token) {
     return access_token;
   }
