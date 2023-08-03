cc0f348b5c2c9012f461b6c1844633103528304c
2DC <kkts9308@gmail.com>
2023-08-03 16:36:54 +0900

    라우터 수정 : 레이아웃 반영

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/Header";
 import Home from "../pages/home/Home";
 import SignUp from "../pages/signup/SignUp";
 import SignIn from "../pages/signin/SignIn";
 import Todo from "../pages/todo/Todo";
+import NotFound from "../pages/error/NotFound";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/todo' element={<Todo />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/todo' element={<Todo />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
